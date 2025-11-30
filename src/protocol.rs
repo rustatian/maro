a1e62002492cf34a15b7e6e36a023ca523a16ebb
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-11-30 21:18:41 +0100

    chore: send and receive
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,17 +1,33 @@
-use serde::de::DeserializeOwned;
 use serde::Serialize;
+use serde::de::DeserializeOwned;
 use tokio::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
-enum Request {}
+enum Request {
+    Add { a: i32, b: i32 },
+    Echo { msg: String },
+}
 
-enum Response {}
+enum Response {
+    Value(i64),
+    Text(String),
+}
 
 pub async fn send<T: Serialize>(stream: &mut TcpStream, msg: &T) -> io::Result<()> {
-    Ok(())
+    let data =
+        serde_json::to_vec(msg).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+    stream.write_all(&data.len().to_be_bytes()).await?;
+    stream.write_all(&data).await?;
+    stream.flush().await
 }
 
 pub async fn recv<T: DeserializeOwned>(stream: &mut TcpStream) -> io::Result<T> {
-    let mut buf = vec![0u8, 10];
+    let mut len_buf = [0u8; 8];
+    stream.read_exact(&mut len_buf).await?;
+    let len = usize::from_be_bytes(len_buf);
+
+    let mut buf = vec![0u8; len];
+    stream.read_exact(&mut buf).await?;
     serde_json::from_slice(&buf).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
 }
